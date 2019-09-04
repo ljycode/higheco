81e950e8f2cfada50847a0d0f8b460ca0ccf0ef4
WS <183158862@qq.com>
2019-09-04 13:37:22 +0800

    new

--- scripts/index.js
+++ scripts/index.js
@@ -9,7 +9,9 @@ $(function(){
     });
     // 轨道交通展开
     $(".unfold").mouseover(function(){
-        $(".nav-box").stop().slideDown();
+        if(window.innerWidth > 767){
+            $(".nav-box").stop().slideDown();
+        }
     });
     $(".unfold").mouseout(function(){
         $(".nav-box").stop().slideUp();
@@ -26,12 +28,36 @@ $(function(){
         }
     });
 
+    // 导航鼠标悬浮添加样式
+    var on_index = $(".nav .on").index();
+    $(".nav .item").mouseover(function(){
+        if(window.innerWidth > 767){
+            $(this).addClass("on").siblings().removeClass("on");
+        }
+    })
+    
+    $(".nav .item").mouseout(function(){
+        if(window.innerWidth > 767){
+            switch(on_index) {
+                case 1: $(".nav .item").eq(0).addClass("on").siblings().removeClass("on");break;
+                case 2: $(".nav .item").eq(1).addClass("on").siblings().removeClass("on");break;
+                case 4: $(".nav .item").eq(2).addClass("on").siblings().removeClass("on");break;
+                case 5: $(".nav .item").eq(3).addClass("on").siblings().removeClass("on");break;
+                case 6: $(".nav .item").eq(4).addClass("on").siblings().removeClass("on");break;
+                case 7: $(".nav .item").eq(5).addClass("on").siblings().removeClass("on");break;
+            }
+        }
+    })
+        
+
     //移动端导航点击事件
     $(".nav-btn").click(function(){
         $(this).prev(".nav-2").show();
     });
     $(".push").click(function(){
-        $(this).next(".nav-box-2").toggle();
+        if(window.innerWidth <= 767){
+            $(this).next(".nav-box-2").toggle();
+        }
     });
     $(".close-btn").click(function(){
         $(this).parent().hide();
