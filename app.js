33f34555a8015d6ee03cb06cd81836b00354ae91
ljycode <ljycode@163.com>
2019-11-27 13:34:31 +0800

    a

--- app.js
+++ app.js
@@ -1,12 +1,36 @@
-
-
-
-const express = require('express')
+//引入express
+const express = require('express');
 const path = require('path')
-const app = express()
-
-app.use(express.static(path.join(__dirname, './')))
-
-app.listen(8080, () => {
-  console.log('App listening at port 8080')
+//监听的端口
+const port = 20888;
+const app = express();
+//引入os
+const os = require('os');
+//获取本机ipv4地址
+function getIPv4() {
+    //同一接口可能有不止一个IP4v地址，所以用数组存
+    let ipv4s = [];
+    //获取网络接口列表对象
+    let interfaces = os.networkInterfaces();
+    Object.keys(interfaces).forEach(function (key) {
+        interfaces[key].forEach(function (item) {
+            //跳过IPv6 和 '127.0.0.1'
+            if ('IPv4' !== item.family || item.internal !== false) return;
+            ipv4s.push(item.address); //可用的ipv4s加入数组
+            // console.log(key + '--' + item.address);
+        })
+    })
+    return ipv4s[0]; //返回一个可用的即可
+}
+let ipv4 = getIPv4();
+//设置静态文件路径
+app.use(express.static(path.join(__dirname, './')));
+//监听端口
+module.exports = app.listen(port, function (err) {
+    if (err) {
+        console.log(err)
+        return
+    } else {
+        console.log('Listening at http://localhost:' + port + '\n' + 'or at http://' + ipv4 + ':' + port)
+    }
 })
